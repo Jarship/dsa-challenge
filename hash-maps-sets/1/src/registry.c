76c594ad837a9ff9cba8869a8564eb7bc2e10898
John Bishop <johnarthurbishop@gmail.com>
2026-09-01 14:18:27 -0400

    fix: re-hash uses i; fix: calloc; fix: handle collisions when re-hashing

--- hash-maps-sets/1/src/registry.c
+++ hash-maps-sets/1/src/registry.c
@@ -25,12 +25,13 @@ void registry_add(Registry *r, int id) {
 		if (r-> count <= 0)
 			new_slots = realloc(r->slots, sizeof(Slot) * new_cap);
 		else {
-        		new_slots = calloc(new_cap, new_cap * sizeof(Slot));
+        		new_slots = calloc(new_cap, sizeof(Slot));
 			for (int i = 0; i < r-> capacity; i++) {
-				if (r->slots[index].occupied) {
+				if (r->slots[i].occupied) {
 					int new_index = calculate_new_index(r->slots[index].value, new_cap);
-					new_slots[new_index] = r->slots[index];
-					index = registry_calculate_index(r, (index + 1) % r->capacity);
+					while(new_slots[new_index].occupied) new_index = (new_index + 1) % new_cap;
+					new_slots[new_index] = r->slots[i];
+					index = (index + 1) % r->capacity;
 				}
 			}
 		}
